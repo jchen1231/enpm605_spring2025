ffd1545d722c37beea41f05a52c65e4efebcaa71
jchen1231 <jasonchen4298@gmail.com>
2025-03-17 01:06:13 -0400

    File structure for RWA3

---
+++ rwa3_lastname/space_robotics/main.py
@@ -0,0 +1,38 @@
+"""
+# Program Testing
+
+In `main()`, implement the following code to test different aspects of your program:
+  - Create a `PlanetaryRover` robot:
+    - Name: `MarsExplorer`
+    - Mobility: `wheels`
+    - Terrain: `rocky`
+    - Sensors:
+      - LiDAR: `"X-500"`, `100.0`, `0.5`
+      - LiDAR: `"OS1"`, `20.5`, `1.5`
+      - Camera: `"CamPro"`, `50.0`, `20`
+  - Create an `OrbitalRobotRover` robot:
+    - Name: `OrbitFixer`
+    - Mobility: `thrusters`
+    - Altitude: `400.5`
+    - Sensors:
+      - LiDAR: `"X-500"`, `100.0`, `0.5`
+      - Camera: `"CamPro"`, `50.0`, `20`
+      - Camera: `"OrbitCam"`, `20.0`, `20`
+  - Create a robotic fleet
+  - Add both robots to the fleet
+  - Report the status of the fleet
+  - Activate the sensors for all robots in the fleet
+  - Deploy mission `mapping`, `collection`, `repair`, and `maintenance`
+  - Remove `OrbitalRobotRover` from the fleet
+  - Deploy mission `maintenance` and `mapping`
+  - Report the status of the fleet
+  
+Note: If I create a third robot and I add it to the fleet, your fleet operations should
+still work (use for loop)
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
